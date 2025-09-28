50a31da3b270172e4fe954e96a80c2d413844d4f
ksj321456 <ksj321456@gmail.com>
2025-09-29 02:34:42 +0900

    5장 14번 문제 풀이

--- src/ch05/Main.java
+++ src/ch05/Main.java
@@ -58,6 +58,7 @@ public class Main {
                 scanner.close();
                 System.out.println("프로그램을 종료합니다.");
                 System.exit(0);
+                //
             }
         }
     }
